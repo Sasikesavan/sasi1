eea1b3a12db7fba4f37832fdb97d8ae3b4fc7500
Sasikesavan <sasiprabakesavan@gmail.com>
2016-07-10 12:53:11 +0530

    Create prog5.c

---
+++ prog5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+main()
+{    
+int number[]={31,14,15,7,2};;
+int i, j, a, n;
+
+for (i = 0; i < 4; ++i)
+   {
+   for (j = i + 1; j < 4; ++j)
+   {
+         if (number[i] < number[j])
+            {
+            a = number[i];
+    number[i] = number[j];
+    number[j] = a;
+}
+}
+}
+    printf("The numbers arranged in descending order are given below\n");
+    for (i = 0; i < 4; ++i)
+    {
+        printf("%d\n", number[i]);
+    }
+    }
